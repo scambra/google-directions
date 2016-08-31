72ecc0635f0e6fb9610846ed35174002c895a53e
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2016-08-31 18:46:00 -0300

    Adds ability to sign Google Maps API request

--- lib/google/directions/request.rb
+++ lib/google/directions/request.rb
@@ -9,14 +9,16 @@ module Google
       attr_reader :params, :response
 
       def initialize
-        session.base_url        = (private_key ? 'https://' : 'http://') + BASE_URL
+        session.base_url        = ((sign? || private_key) ? 'https://' : 'http://') + BASE_URL
         session.timeout         = Google::Directions.config.timeout
         session.connect_timeout = Google::Directions.config.connect_timeout
       end
 
       def get(params)
         @params   = params.with_indifferent_access
-        @response = session.get GET_PATH + '?' + parse_params.to_query
+
+        url = sign_url_if_needed(GET_PATH + '?' + parse_params.to_query)
+        @response = session.get url
 
         response ? ::JSON.parse(response.body) : nil
       end
@@ -38,6 +40,10 @@ module Google
         parsed_params
       end
 
+      def sign_url_if_needed(uri_with_params)
+        sign? ? Encoder.new(uri_with_params, private_key).encode : uri_with_params
+      end
+
       def parse_waypoints
         optimize = !!params[:optimize] || false
         waypoints = params[:waypoints].join('|')
@@ -46,7 +52,14 @@ module Google
       end
 
       def set_auth_params!(parsed_params)
-        parsed_params[:key] = private_key if private_key
+        if sign?
+          missing('client_id') unless client_id
+          missing('private_key') unless private_key
+
+          parsed_params[:client] = client_id
+        elsif private_key
+          parsed_params[:key] = private_key
+        end
       end
 
       def session
@@ -59,6 +72,14 @@ module Google
 
       def private_key
         Google::Directions.config.private_key
+      end
+
+      def client_id
+        Google::Directions.config.client_id
+      end
+
+      def sign?
+        Google::Directions.config.sign
       end
     end
   end
